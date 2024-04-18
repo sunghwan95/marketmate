993f84863895ac16b51e65dde589321207c7c209
성환 <psung9510@gmail.com>
2024-04-18 17:50:32 +0900

    Init: Create Repository

---
+++ src/main/java/com/hwann/marketmate/MarketmateApplication.java
@@ -0,0 +1,11 @@
+package com.hwann.marketmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketmateApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MarketmateApplication.class, args);
+	}
+}
